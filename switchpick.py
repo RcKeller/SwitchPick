a4207803339fe741f34e644c9b1def1d1847da84
Ryan Keller <rykeller@uw.edu>
2016-06-09 00:13:26 -0700

    Add files via upload
    Added file browser, refactored primary algorithms.

--- switchpick.py
+++ switchpick.py
@@ -45,8 +45,13 @@
 ################################################################################
 
 import serial
+import easygui
 import sys, os
 import time
+
+from Tkinter import *
+import tkFileDialog
+from tkFileDialog import askopenfilename # Open dialog box
 
 CONSOLE = ''        #Our serial connection will be a global value
 USERNAME = ''
@@ -54,9 +59,10 @@ PASSWORD = ''
 ENCRYPTED_PASSWORD = ''
 READ_TIMEOUT = 8
 
-CREDENTIAL_FILE = (os.path.dirname(__file__) + '\\data.txt')
-GENERAL_CONFIG = (os.path.dirname(__file__) + '\\general.config')
-PROVISIONING_LOG = (os.path.dirname(__file__) + '\\provisioning.csv')
+CREDENTIAL_FILE = os.path.join(os.path.dirname(sys.argv[0]), 'data', 'data.txt')
+GENERAL_CONFIG = os.path.join(os.path.dirname(sys.argv[0]), 'configs', 'prime.config')
+PROVISIONING_LOG = os.path.join(os.path.dirname(sys.argv[0]), 'records', 'NIM_log.csv')
+
 
 BLACKBOX = True     #Blackbox mode is default, False is used for debugging
 
@@ -68,40 +74,56 @@ def main():
     initializeSerialPort()
     loadCredentials()
     while True:
-        menu()
-        choice = option(0, 6)
-        
-        if choice == 0:     #Exit Script
-            sys.exit()
         try:
-			if choice == 1:   #Credentials
-				credentials()
-			elif choice == 3:   #Provisioning
-				provisioningMenu()
-				choice = option(0, 2)
-				if (choice == 1):
-					provisioningLog()
-				elif (choice == 2):
-					clearProvisioningLog()
+            menu()
+            choice = option(0, 6)
+        
+            if choice == 0:     #Exit Script
+                sys.exit()
+            
+            if choice == 1:   #Credentials
+                credentials()
+            elif choice == 2:   #Configure
+                configMenu()
+                choice = option(0, 2)
+                if choice == 1:     #Primer
+                    loadConfig(GENERAL_CONFIG)
+                elif choice == 2:   #Custom Config
+                    file = fileName()
+                    if (file != '') and (file != '.'):
+                        loadConfig(file)
 
-			elif choice == 4:   #Gather logs
-					logs()
+            elif choice == 3:   #Provisioning
+                logExists = provisioningMenu()
+                if (logExists == True):
+                    choice = option(0, 2)
+                    if (choice == 1):
+                        provisioningLog()
+                    elif (choice == 2):
+                        clearProvisioningLog()
+                else:
+                    print ('No logs found - creating and clearing a new record')
+                    clearProvisioningLog()
 
-			elif choice == 5:   #Wipe
-				wipeMenu()
-				choice = option(0, 2)
-				if (choice != 0):
-					wipe(choice)
+            elif choice == 4:   #Gather logs
+                    logs()
 
-			elif choice == 6:   #Shutdown
-				powerMenu()
-				choice = option(0, 2)
-				if (choice != 0):
-					powerOptions(choice)
-                    
-        except Exception as reason:
-            returnException(reason)
+            elif choice == 5:   #Wipe
+                wipeMenu()
+                choice = option(0, 2)
+                if (choice != 0):
+                    wipe(choice)
 
+            elif choice == 6:   #Shutdown
+                powerMenu()
+                choice = option(0, 2)
+                if (choice != 0):
+                    powerOptions(choice)
+                
+        except SystemExit:
+            return
+        except KeyboardInterrupt:
+            print ('Process interupped by keyboard - returning to menu')
             
             
 ################################################################################
@@ -111,17 +133,28 @@ def main():
 def menu():
     print('\n\n')
     print('='*40)
-    print('SwitchPick for JUNOS')
+    print('SwitchPick for JUNOS - Beta Version')
     print('\tCode by Keller, UW-IT NIM')
+    print('\tConfig by Norm, UW-IT NIM')
     print('-'*40)
     print('USER: ' + USERNAME + '\tPASS: ' + ('*' * len(PASSWORD)) + '\nEncryption Loaded:\t' + str(len(ENCRYPTED_PASSWORD) > 1))
     print('='*40)
     print('\t1) Update Credentials')
-    print('\t*) Configuration')
+    print('\t2) Configuration')
     print('\t3) Provisioning')
     print('\t4) Generate Logs')
     print('\t5) Wipe Settings')
     print('\t6) Power Options')
+    print('='*40)
+    return
+    
+    
+def configMenu():
+    print('-'*40)
+    print('Switch Config | Process begins at login prompt')
+    print('-'*40)
+    print('\t1) Priming Config')
+    print('\t2) Custom Config')
     print('='*40)
     return
     
@@ -136,19 +169,26 @@ def wipeMenu():
     print('='*40)
     return
     
-    
 def provisioningMenu():
     print('-'*40)
     print('Provisioning Logs | Records of switch deployments')
     print('File: ' + PROVISIONING_LOG)
-    if (os.path.exists(PROVISIONING_LOG) == False):
-        raise Exception('File not found, no data to read/clear.')
-    print('-'*40)
-    print('\t1) Read Logs')
-    print('\t2) Clear Logs')
-    print('='*40)
-    return
     
+    fileExists = False
+    try:
+        if (os.path.exists(PROVISIONING_LOG) == False):
+            print('-'*40)
+            raise Exception('File not found, no data to read/clear.')
+        else:
+            fileExists = True
+        print('-'*40)
+        print('\t1) Read Logs')
+        print('\t2) Clear Logs')
+        print('='*40)
+    except Exception as reason:
+        fileExists = False
+            
+    return fileExists
     
 def powerMenu():
     print('-'*40)
@@ -201,65 +241,88 @@ def credentials():
     global USERNAME, PASSWORD
     USERNAME = raw_input('Username: ').rstrip('\n')
     PASSWORD = raw_input('Password: ').rstrip('\n')
-    return 
+    return
     
+
+def loadConfig(configFile):
+    print('-'*40)
+    print('Loading Config:\n' + configFile)
+    print('-'*40)
+
+    print('This is a sample distribution of this program.')
+    print('Configuration not available.')
+
+    return    
+
     
 def provisioningLog():
     print('-'*80)
-    file = open(PROVISIONING_LOG, 'r')
-    line = file.readline().rstrip('\n')
-    returnLine = ''
-    while (line != ''):
-        line = line.replace(',', '').split()
-        for i in range(0, len(line)):
-			# line[i] = line[i].rstrip(',')
-            returnLine += ('{:20s}'.format(line[i]))
-        print(returnLine)
-        returnLine = ''
-        line = file.readline()
-    print('-'*80)
-    return
     
+    try:
+    
+        file = open(PROVISIONING_LOG, 'r')
+        line = file.readline().rstrip('\n')
+        returnLine = ''
+        while (line != ''):
+            line = line.replace(',', '').split()
+            for i in range(0, len(line)):
+                # line[i] = line[i].rstrip(',')
+                returnLine += ('{:20s}'.format(line[i]))
+            print(returnLine)
+            returnLine = ''
+            line = file.readline()
+            print('-'*80)
+            
+    except Exception as reason:
+            returnException(reason)
+    
+    return
     
 def logs():
     print('-'*40)
     print('Log Generator | Copies log files to USB drive')
     print('-'*40)
     
-    goToLogin()
-    login()
-    cli()
-    command('}', 'request support information | save /var/tmp/RSI.txt', 'Generating RSI files (2 minutes)', False)
-    command('}', 'file archive source /var/log destination /var/tmp/LOGS', 'Generating LOG file (30 seconds)', False)
-    command('}', 'start shell', 'Moving to shell mode', False)
+    try:
     
-    #Find a drive, mount it, and ensure it is functional
-    print('Searching for Drive...')
-    while True:     #Requires a special loop that command() cannot accomodate
-        time.sleep(1)
-        response = readSerial()
-        if ('%' in response):
-            CONSOLE.write('mount_msdosfs /dev/da1s1 /mnt' + '\n')
-            time.sleep(5)
+        goToLogin()
+        login()
+        cli()
+        command('}', 'request support information | save /var/tmp/RSI.txt', 'Generating RSI files (2 minutes)', False)
+        command('}', 'file archive source /var/log destination /var/tmp/LOGS', 'Generating LOG file (30 seconds)', False)
+        command('}', 'start shell', 'Moving to shell mode', False)
+        
+        #Find a drive, mount it, and ensure it is functional
+        print('Searching for Drive...')
+        while True:     #Requires a special loop that command() cannot accomodate
+            time.sleep(1)
             response = readSerial()
-            if ('not permitted' in response):
-                goToLogin()
-                raise Exception('Fatal Error - insufficient permission, unable to mount drives.')
-            elif ('no such' not in response):   #There isn't a success message
-                print('Drive mounted to /mnt')
-                break
-            else:
-                print('...No drive found. Retrying in 15 seconds...')
-                time.sleep(10)
-            CONSOLE.write('\n') #Priming for a new loop
-    command('%', 'cp /var/tmp/RSI.txt /mnt', 'Copying RSI files')
-    command('%', 'cp /var/tmp/LOGS.tar /mnt', 'Copying LOG files', False)
-    time.sleep(5)
-    command('%', 'umount /mnt', '\nLogs copied! Unmounting drive /mnt', False)    #umount != unmount
-    time.sleep(1)
-    print('Logging out for security.')
-    goToLogin()
-    print('Process complete, console at the login screen.')
+            if ('%' in response):
+                CONSOLE.write('mount_msdosfs /dev/da1s1 /mnt' + '\n')
+                time.sleep(5)
+                response = readSerial()
+                if ('not permitted' in response):
+                    goToLogin()
+                    raise Exception('Fatal Error - insufficient permission, unable to mount drives.')
+                elif ('no such' not in response) and ('%' in response):   #There isn't a success message
+                    print('Drive mounted to /mnt')
+                    break
+                else:
+                    print('...No drive found. Retrying in 15 seconds...')
+                    time.sleep(10)
+                CONSOLE.write('\n') #Priming for a new loop
+        command('%', 'cp /var/tmp/RSI.txt /mnt', 'Copying RSI files')
+        command('%', 'cp /var/tmp/LOGS.tar /mnt', 'Copying LOG files', False)
+        time.sleep(5)
+        command('%', 'umount /mnt', '\nLogs copied! Unmounting drive /mnt', False)    #umount != unmount
+        time.sleep(1)
+        print('Logging out for security.')
+        goToLogin()
+        print('Process complete, console at the login screen.')
+    
+    except Exception as reason:
+            returnException(reason)
+    
     return
 
     
@@ -267,56 +330,70 @@ def wipe(choice):
     print('-'*40)
     print('Clean Config Wipe | Retains System Logs')
     print('-'*40)
-        
-    #Reaching shell, either from login or loader
-    if (choice == 1): #Login Screen
-        goToLogin()
-        login()
-    else:             #Not at login, assuming we're booting
-        loader()
-        CONSOLE.write('boot -s\n')
-        print('Booting in single user mode (1 minute)...')
-        command('root password recovery', 'recovery', 'Starting password recovery, (4 minutes)...', False)
-        command('}', 'start shell', 'Starting Shell...', False)
-        
-    command('%', 'cd /config', 'Directory: /config')
-    command('%', 'rm juniper.conf.gz', '\tRemoving: juniper.conf.gz')
-    command('%', 'rm juniper.conf.*.gz', '\tRemoving: juniper.conf.*.gz')
-    command('%', 'rm rescue.conf.gz', '\tRemoving: rescue.conf.gz')
-    command('%', 'cd /var/run/db', 'Directory: /var/run/db')
-    command('%', 'rm juniper.db', '\tRemoving: juniper.db')
-    command('%', 'rm juniper.data', '\tRemoving: juniper.data')
-    command('%', 'rm juniper.save', '\tRemoving: juniper.save')
-    print('Wipe complete!')
     
-    print('-'*40 + '\nWipe complete. Reboot?\n' + '-'*40)
-    print('\t1) Leave as is')
-    print('\t2) Power off (graceful)')
-    print('\t3) Full reboot')
-    print('-'*40)
-    choice = option(0, 3)
-    print('-'*40)
-    cli()
-    if (choice == 2):
-        powerOff()
-    elif (choice == 3):
-        reboot()
-    else:
-        print('Returning to menu.')
+    try:
+        
+        #Reaching shell, either from login or loader
+        if (choice == 1): #Login Screen
+            goToLogin()
+            login()
+        else:             #Not at login, assuming we're booting
+            loader()
+            CONSOLE.write('boot -s\n')
+            print('Booting in single user mode (1 minute)...')
+            command('root password recovery', 'recovery', 'Starting password recovery, (4 minutes)...', False)
+            command('}', 'start shell', 'Starting Shell...', False)
+            
+        command('%', 'cd /config', 'Directory: /config')
+        command('%', 'rm juniper.conf.gz', '\tRemoving: juniper.conf.gz')
+        command('%', 'rm juniper.conf.*.gz', '\tRemoving: juniper.conf.*.gz')
+        command('%', 'rm rescue.conf.gz', '\tRemoving: rescue.conf.gz')
+        command('%', 'cd /var/run/db', 'Directory: /var/run/db')
+        command('%', 'rm juniper.db', '\tRemoving: juniper.db')
+        command('%', 'rm juniper.data', '\tRemoving: juniper.data')
+        command('%', 'rm juniper.save', '\tRemoving: juniper.save')
+        print('Wipe complete!')
+        
+        print('-'*40 + '\nWipe complete. Reboot?\n' + '-'*40)
+        print('\t1) Leave as is')
+        print('\t2) Power off (graceful)')
+        print('\t3) Full reboot')
+        print('-'*40)
+        choice = option(0, 3)
+        print('-'*40)
+        if (choice == 2):
+            cli()
+            powerOff()
+        elif (choice == 3):
+            cli()
+            reboot()
+        else:
+            print('Returning to menu.')
+        
+    except Exception as reason:
+            returnException(reason)
+    
     return
         
 
 def powerOptions(choice):
+    print('-'*40)
     if (choice == 0):
         print('Returning to menu.')
     else:
-        goToLogin()
-        login()
-        cli()
-        if (choice == 1):
-            powerOff()
-        elif (choice == 2):
-            reboot()
+        try:
+        
+            goToLogin()
+            login()
+            cli()
+            if (choice == 1):
+                powerOff()
+            elif (choice == 2):
+                reboot()
+        
+        except Exception as reason:
+            returnException(reason)
+    
     return
     
     
@@ -339,7 +416,6 @@ def loader():
             break
     return
 
-    
 def goToLogin():
     loginPrompt = False
     while loginPrompt != True:
@@ -402,8 +478,7 @@ def goodCommit():
             if (BLACKBOX != True):
                 print('REASON:' + response)
             print('-'*40)
-            print('Error: This commit has failed')
-            print('This will require manual troubleshooting, please console in')
+            print('Error: This commit has failed - This will require manual troubleshooting')
             print('-'*40)
             return False
             
@@ -437,6 +512,7 @@ def reboot():
 ################################################################################
 
 def initializeSerialPort():
+    #Loops until able to connect to serial ports.
     print('-'*40)
     print('Auto-Initializing Serial Port: 9600 8-N-1')
     connected = False
@@ -463,12 +539,14 @@ def initializeSerialPort():
             if serialNumber < 0:
                 if errorPrinted == False:
                     print('\tUnable to connect, please reseat adapaters')
+                    print('-'*40)
                 serialNumber = 9
                 time.sleep(10)  #Loops until a successful connection can be established.
     return
                 
 
 def readSerial():
+    #Reads and returns all the bytes / characters waiting in the serial buffer.
     dataBytes = CONSOLE.inWaiting()
     if dataBytes:
         return CONSOLE.read(dataBytes)
@@ -478,17 +556,18 @@ def readSerial():
         
 def command(condition, command, reaction='', pullPrompt=True, newLine=True):
     while True:
+        #Loops until conditions are met.
         if (pullPrompt == True):
+            #Press enter every second to try and "pull" output from a switch
             CONSOLE.write('\n')
         time.sleep(1)
         response = readSerial()
-        if (BLACKBOX != True):
+        if (BLACKBOX != True): #For debugging
             print(response)
         if (condition in response):
+            #Adding newlines to commands is optional, some commands may not need them
             CONSOLE.write(command + ('\n' if newLine else ''))
             print(reaction)
-            if (BLACKBOX != True):
-                print('SwitchPick: ' + command)
             break
     return
     
@@ -507,6 +586,8 @@ def option(low, high):
                 break
             else:
                 print('Option must be between ' + low + '-' + high)
+        except KeyboardInterrupt as reason:
+            sys.exit()
         except:
             print('Option not accepted')
     return option
@@ -514,23 +595,22 @@ def option(low, high):
     
 def fileName():
     #Loops until a valid .txt file is specified
-    while True:
         try:
-            name = raw_input('File Name >    ')
-            if (name == '0'):
-                return name
-            #if (name[-4:] != '.txt') and (name[-7:] != '.config'):
-            if (not name.endswith('.txt')) and (not name.endswith('.config')):
-                name = (name + '.txt')
-            if ('/' not in name) or ('\\' not in name):
-                name = os.path.dirname(__file__) + '\\' + name
-            r = open(name, 'r')
-            data = r.read()
-            if data != '':
+            while True:
+                init = Tk()
+                init.withdraw()
+                name = askopenfilename()
+                if (name == '.') or (name == ''):
+                    raise Exception('No file specified.')
+                r = open(name, 'r')
+                data = r.read()
+                if data == '':
+                    raise Exception('No data contained in: ' + name)
                 r.close()
                 return name
-        except:
-            print('An error occured - enter a different name:')
+        except Exception as reason:
+            returnException(reason)
+            return ''
     
     
 def appendProvisioningLog(name, mac, ip, sub):
@@ -545,7 +625,6 @@ def appendProvisioningLog(name, mac, ip, sub):
     print('SUB\t| ' + sub)
     print('-'*40)
     return
-    
     
 def clearProvisioningLog():
     file = open(PROVISIONING_LOG, 'w')
@@ -569,5 +648,3 @@ def returnException(reason):
 ################################################################################
 
 main()
-
-
